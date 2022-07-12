a7720cb6c6f46f2a01732018f99c2ee6b06f8803
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-12 18:57:16 +0500

    Data Procesing Script

--- process_data.R
+++ process_data.R
@@ -4,3 +4,23 @@
 library(foreign)
 library(arrow)
 library(tidyverse)
+
+t <- tempdir()
+
+sas_files <- list.files("sas_data/", full.names = TRUE)
+
+sas_file_names <- list.files("sas_data/") |>
+    str_remove(".zip")
+
+xpt_files <- sas_files |>
+    map_chr(~ utils::unzip(.x, exdir = t)) |>
+    set_names(sas_file_names)
+
+data_files <- xpt_files |>
+    map(~ read.xport(.x)) |>
+    {
+        \(x) map2(x, names(x), ~ saveRDS(.x, file.path("rds", str_glue("{.y}.RDS"))))
+    }()
+
+unlink(xpt_files)
+
