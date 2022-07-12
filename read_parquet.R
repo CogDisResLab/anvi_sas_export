a986049738c6aa99e2da86115fb236604649b0a0
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-12 19:53:09 +0500

    Incidence Data

--- read_parquet.R
+++ read_parquet.R
@@ -4,14 +4,30 @@
 library(arrow)
 library(tidyverse)
 
+state_codes <- read_csv("sas_state_code.csv") |>
+    rename(STATE = CODE)
+
 ccaei103 <- open_dataset("parquet/ccaei103") |>
-    select(ENROLID, depression, AGE) |>
+    select(ENROLID, STATE, schizophrenia, AGE) |>
     filter(AGE <= 65, AGE >= 18) |>
-    group_by(depression) |>
+    group_by(STATE, schizophrenia) |>
     distinct() |>
     collect() |>
-    summarise(num = sum(depression)) |>
-    filter(depression == 1)
+    summarise(num = sum(schizophrenia)) |>
+    filter(schizophrenia == 1) |>
+    select(-schizophrenia) |>
+    inner_join(state_codes) |>
+    ungroup() |>
+    select(-STATE) |>
+    rename(STATE = NAME,
+           CASES = num)
 
-ccaea10a <- open_dataset("parquet/ccaea10a") |>
-    collect()
+population <- read_csv("nst-est2019-alldata.csv") |>
+    filter(SUMLEV == "040") |>
+    select(NAME, CENSUS2010POP) |>
+    rename(STATE = NAME) |>
+    mutate(STD_POP = CENSUS2010POP / 100000)
+
+rates <- inner_join(population, ccaei103) |>
+    mutate(rates = round(CASES / STD_POP, 4)) |>
+    write_csv("schizophrenia_incidence_rates_population.csv")
