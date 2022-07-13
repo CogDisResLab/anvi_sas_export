7693ae852a2f2a6bfbd240c2f3c0bb22340d9b1e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-13 23:43:03 +0500

    Dumped data and scripts

---
+++ match_patients.R
@@ -0,0 +1,2 @@
+# Get matched patients
+
--- process_by_msa.R
+++ process_by_msa.R
@@ -4,25 +4,25 @@
 library(arrow)
 library(tidyverse)
 
-state_codes <- read_csv("raw/sas_state_code.csv") |>
+state_codes <- read_csv("data/sas_state_code.csv") |>
     rename(STATE = CODE)
 
 ccaei103 <- open_dataset("parquet/ccaei103") |>
-    select(ENROLID, STATE, depression, AGE) |>
+    select(ENROLID, MSA, schizophrenia, AGE) |>
     filter(AGE <= 65, AGE >= 18) |>
-    group_by(STATE, depression) |>
+    group_by(STATE, schizophrenia) |>
     distinct() |>
     collect() |>
-    summarise(num = sum(depression)) |>
-    filter(depression == 1) |>
-    select(-depression) |>
+    summarise(num = sum(schizophrenia)) |>
+    filter(schizophrenia == 1) |>
+    select(-schizophrenia) |>
     inner_join(state_codes) |>
     ungroup() |>
     select(-STATE) |>
     rename(STATE = NAME,
            CASES = num)
 
-population <- read_csv("raw/nst-est2019-alldata.csv") |>
+population <- read_csv("data/nst-est2019-alldata.csv") |>
     filter(SUMLEV == "040") |>
     select(NAME, CENSUS2010POP) |>
     rename(STATE = NAME) |>
@@ -30,4 +30,4 @@ population <- read_csv("raw/nst-est2019-alldata.csv") |>
 
 rates <- inner_join(population, ccaei103) |>
     mutate(rates = round(CASES / STD_POP, 4)) |>
-    write_csv("results/depression_incidence_rates_population.csv")
+    write_csv("results/schizophrenia_incidence_rates_population.csv")
