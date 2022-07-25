765b5a42db888c3ead56558d3586d347c4cd9346
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-25 22:05:46 +0500

    patient matchign script added

--- match_patients.R
+++ match_patients.R
@@ -1,2 +1,51 @@
 # Get matched patients
+#
+#
+#
 
+library(MatchIt)
+library(tidyverse)
+library(arrow)
+
+
+# Matched Depression
+dataset <- open_dataset("parquet/ccaei103") |>
+    select(ENROLID, depression, AGE, SEX) |>
+    filter(AGE <= 65, AGE >= 18) |>
+    distinct() |>
+    collect() |>
+    mutate(SEX = as.numeric(SEX) - 1) |>
+    drop_na(ENROLID, AGE, SEX) |>
+    rename_with(str_to_upper)
+
+
+if (file.exists("datastore/matched_ibm_depression.RData")) {
+    load("datastore/matched_ibm_depression.RData")
+} else {
+    model <- matchit(DEPRESSION ~ SEX + AGE, data = dataset)
+    save(model, file = "datastore/matched_ibm_depression.RData")
+}
+
+matched <- match.data(model) |>
+    write_csv("results/matched/matched_depression.csv")
+
+# Matched Enteritis
+dataset <- open_dataset("parquet/ccaei103") |>
+    select(ENROLID, regional_enteritis, AGE, SEX) |>
+    filter(AGE <= 65, AGE >= 18) |>
+    distinct() |>
+    collect() |>
+    mutate(SEX = as.numeric(SEX) - 1) |>
+    drop_na(ENROLID, AGE, SEX) |>
+    rename_with(str_to_upper)
+
+
+if (file.exists("datastore/matched_ibm_enteritis.RData")) {
+    load("datastore/matched_ibm_enteritis.RData")
+} else {
+    model <- matchit(REGIONAL_ENTERITIS ~ SEX + AGE, data = dataset)
+    save(model, file = "datastore/matched_ibm_enteritis.RData")
+}
+
+matched <- match.data(model) |>
+    write_csv("results/matched/matched_regional_enteritis.csv")
