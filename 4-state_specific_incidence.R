24d6a5517549f4c852dcd4cdc33790ab67ebeb41
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-27 01:51:45 +0500

    Parametrized incidences and adopted consistent result file naming

--- 4-state_specific_incidence.R
+++ 4-state_specific_incidence.R
@@ -7,45 +7,55 @@ library(tidyverse)
 state_codes <- read_csv("raw/sas_state_code.csv") |>
     rename(STATE = CODE)
 
-depression_matched <- read_csv("results/matched/matched_depression.csv") |>
-    pull(ENROLID)
+population <- read_csv("ancillary/standardized_population.csv")
 
-ccaei103 <- open_dataset("parquet/ccaei103") |>
-    select(ENROLID, STATE, depression, AGE) |>
-    filter(AGE <= 65, AGE >= 18,
-           ENROLID %in% depression_matched) |>
-    group_by(STATE, depression) |>
-    distinct() |>
-    collect() |>
-    summarise(num = sum(depression)) |>
-    filter(depression == 1) |>
-    select(-depression) |>
-    inner_join(state_codes) |>
-    ungroup() |>
-    select(-STATE) |>
-    rename(STATE = NAME,
-           CASES = num)
+calculate_state_incidences <- function(disease) {
 
-population <- read_csv("raw/nst-est2019-alldata.csv") |>
-    filter(SUMLEV == "040") |>
-    select(NAME, CENSUS2010POP) |>
-    rename(STATE = NAME,
-           CENSUSPOP = CENSUS2010POP) |>
-    mutate(STD_POP_CS = CENSUSPOP / 100000)
+    matched_data_file <- file.path(
+        "results",
+        "matched",
+        str_glue("matched_{disease}.csv")
+    )
 
-population <- open_dataset("parquet/ccaei103") |>
-    count(STATE) |>
-    collect() |>
-    inner_join(state_codes) |>
-    ungroup() |>
-    select(-STATE) |>
-    rename(STATE = NAME,
-           DS_POP = n) |>
-    mutate(STD_POP_DS = DS_POP / 1000) |>
-    inner_join(population)
+    rate_output_file <- file.path(
+        "results",
+        str_glue("{disease}_incidence_rates_state.csv")
+    )
 
-rates <- inner_join(population, ccaei103) |>
-    mutate(DS_RATE = round(CASES / STD_POP_DS, 4),
-           CS_RATE = round(CASES / STD_POP_CS, 4)) |>
-    select(STATE, CASES, CENSUSPOP, DS_POP, CS_RATE, DS_RATE) |>
-    write_csv("results/depression_matched_incidence_rates_population.csv")
+    matched <- read_csv(matched_data_file) |>
+        pull(ENROLID)
+
+    ccaei103 <- open_dataset("parquet/ccaei103") |>
+        select(ENROLID, STATE, !!disease, AGE) |>
+        filter(AGE <= 65, AGE >= 18,
+               ENROLID %in% matched) |>
+        rename(disease := !!disease) |>
+        group_by(STATE, disease) |>
+        distinct() |>
+        collect() |>
+        summarise(num = sum(disease)) |>
+        filter(disease == 1) |>
+        select(-disease) |>
+        inner_join(state_codes) |>
+        ungroup() |>
+        select(-STATE) |>
+        rename(STATE = NAME,
+               CASES = num)
+
+    rates <- inner_join(population, ccaei103) |>
+        mutate(DS_RATE = round(CASES / STD_POP_DS, 4),
+               CS_RATE = round(CASES / STD_POP_CS, 4)) |>
+        select(STATE, CASES, CENSUSPOP, DS_POP, CS_RATE, DS_RATE) |>
+        write_csv(rate_output_file)
+
+    rates
+
+}
+
+diseases <- c("depression", "regional_enteritis")
+
+diseases |>
+    walk(calculate_state_incidences)
+
+
+
