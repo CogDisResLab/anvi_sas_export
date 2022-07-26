c0bf6299b67edd508a2788abd48e3dad2ff13193
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-07-26 23:11:20 +0500

    Tried a different strategy for one-hot encoding diagnoses

--- parquet_conversion.R
+++ parquet_conversion.R
@@ -13,30 +13,25 @@ ccaei103_clean <- ccaei103 |>
     select(any_of(variables)) |>
     drop_na(ENROLID) |>
     pivot_longer(starts_with("DX"), names_to = "DXID", values_to = "DX") |>
-    mutate(alcohol_dependence = if_else(DX %in% alcohol_dependence |
-                                            PDX %in% alcohol_dependence, 1, 0),
-           anxiety = if_else(DX %in% anxiety |
-                                            PDX %in% anxiety, 1, 0),
-           bipolar = if_else(DX %in% bipolar |
-                                            PDX %in% bipolar, 1, 0),
-           depression = if_else(DX %in% depression |
-                                            PDX %in% depression, 1, 0),
-           diabetes = if_else(DX %in% diabetes |
-                                            PDX %in% diabetes, 1, 0),
-           drug_dependence = if_else(DX %in% drug_dependence |
-                                            PDX %in% drug_dependence, 1, 0),
-           hypertension = if_else(DX %in% hypertension |
-                                            PDX %in% hypertension, 1, 0),
-           lipid_metabolism = if_else(DX %in% lipid_metabolism |
-                                            PDX %in% lipid_metabolism, 1, 0),
-           obesity = if_else(DX %in% obesity |
-                                            PDX %in% obesity, 1, 0),
-           regional_enteritis = if_else(DX %in% regional_enteritis |
-                                            PDX %in% regional_enteritis, 1, 0),
-           schizophrenia = if_else(DX %in% schizophrenia |
-                                            PDX %in% schizophrenia, 1, 0),
-           suicide = if_else(DX %in% suicide |
-                                            PDX %in% suicide, 1, 0)) |>
+    mutate(diagnosis = case_when(
+        DX %in% alcohol_dependence ~ "alcohol_dependence",
+        DX %in% anxiety ~ "anxiety",
+        DX %in% bipolar ~ "bipolar",
+        DX %in% depression ~ "depression",
+        DX %in% diabetes ~ "Diabetes",
+        DX %in% drug_dependence ~ "drug_dependence",
+        DX %in% hypertension ~ "hypertension",
+        DX %in% lipid_metabolism ~ "lipid_metabolism",
+        DX %in% obesity ~ "obesity",
+        DX %in% regional_enteritis ~ "regional_enteritis",
+        DX %in% schizophrenia ~ "schizophrenia",
+        DX %in% suicide ~ "suicide",
+        TRUE ~ NA_character_
+    ),
+    value = if_else(is.na(diagnosis), 0, 1)) |>
+    select(-DX, -DXID) |>
+    distinct() |>
+    pivot_wider(names_from = diagnosis, values_fill = 0) |>
     write_dataset(file.path("parquet", "ccaei103"))
 
 ccaea10a_clean <- ccaea10a |>
