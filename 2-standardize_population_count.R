a15d0daf6b57cf5c856e165c058982afa3897670
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-02-16 16:51:44 -0500

    Generated a cleaner population count

--- 2-standardize_population_count.R
+++ 2-standardize_population_count.R
@@ -4,16 +4,18 @@ library(arrow)
 library(tidyverse)
 
 state_codes <- read_csv("raw/sas_state_code.csv") |>
-    rename(STATE = CODE)
+    rename(STATE = CODE) |>
+    filter(NAME != "Nation")
 
-population <- read_csv("raw/nst-est2019-alldata.csv") |>
+population_all <- read_csv("raw/nst-est2019-alldata.csv") |>
     filter(SUMLEV == "040") |>
     select(NAME, CENSUS2010POP) |>
     rename(STATE = NAME,
-           CENSUSPOP = CENSUS2010POP) |>
-    mutate(STD_POP_CS = CENSUSPOP / 100000)
+           CS_POP = CENSUS2010POP) |>
+    mutate(STD_POP_CS = CS_POP / 100000) |>
+    arrange(STATE)
 
-population <- open_dataset("parquet/ccaei103") |>
+population_dataset <- open_dataset("parquet/ccaei103") |>
     count(STATE) |>
     collect() |>
     inner_join(state_codes) |>
@@ -22,5 +24,8 @@ population <- open_dataset("parquet/ccaei103") |>
     rename(STATE = NAME,
            DS_POP = n) |>
     mutate(STD_POP_DS = DS_POP / 1000) |>
-    inner_join(population) |>
+    select(STATE, DS_POP, STD_POP_DS) |>
+    mutate(STATE = if_else(STATE == "Washington DC", "District of Columbia", STATE)) |>
+    arrange(STATE) |>
+    inner_join(population_all) |>
     write_csv("ancillary/standardized_population.csv")
